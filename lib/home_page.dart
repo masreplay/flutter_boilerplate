a69ca4ad70ef9d210e7a8c02a1c44eeaa000b8e6
Mohammed Atheer <mohammed.atheer@ta3leem.dev>
2025-05-19 16:15:43 +0300

    Add AsyncSnapshotExtension for improved handling of AsyncSnapshot states in HomePage

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/api_client.dart';
+import 'package:flutter_boilerplate/async_snapshot_extension.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -72,21 +73,5 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
     );
-  }
-}
-
-extension AsyncSnapshotExtension<T> on AsyncSnapshot<T> {
-  R when<R>({
-    required R Function() idle,
-    required R Function() loading,
-    required R Function(T data) data,
-    required R Function(Object? error, StackTrace? stackTrace) error,
-  }) {
-    return switch (connectionState) {
-      ConnectionState.waiting || ConnectionState.active => loading(),
-      ConnectionState.done =>
-        hasError ? error(this.error, stackTrace) : data(this.data as T),
-      ConnectionState.none => idle(),
-    };
   }
 }
